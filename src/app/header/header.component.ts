a7d8eb2ff0da3ecf6042119ea5a4f9ea7e4d77be
Vladimir Sabev <vdsabev@gmail.com>
2017-12-17 23:24:35 +0200

    Updated logo & favicon

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { StoreService } from '../store.service';
+
 import { AuthService } from '../auth.service';
+import { StoreService } from '../store.service';
 
 @Component({
   selector: 'Header',
   template: `
     <a class="logo-link" routerLink="/">
-      <img class="logo-image" src="assets/logo.png" alt="Отвъд Спорта Лого" />
+      <img class="logo-image" src="assets/logo.svg" alt="Отвъд Спорта Лого" />
     </a>
 
     <a routerLink="/">Добави Игрище</a>
@@ -20,20 +21,14 @@ import { AuthService } from '../auth.service';
     <a *ngIf="(store.user$ | async) == null" routerLink="/login">Вход</a>
 
     <ng-container *ngIf="(store.user$ | async) != null">
-      <!-- <a routerLink="/settings">Настройки</a>
-      &nbsp;|&nbsp; -->
       <a *ngIf="(store.user$ | async) != null" routerLink="/" (click)="logout()">Изход</a>
     </ng-container>
   `,
   styles: [`
     :host {
-      grid-row: 1;
-      grid-column: 1;
-
       display: flex;
       align-items: center;
-      background: var(--primary);
-      color: var(--neutral-lighter);
+      background: var(--neutral-lighter);
     }
 
     .logo-link {
